dabb0f0ccd3bc30da2175f332546575cf6d8f62e
Aymeric <aymeric.gioia@hotmail.fr>
2017-01-06 15:42:03 +0100

    first co

--- panda.js
+++ panda.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 		var tendance = this.getAttribute("data-tendance");
 
 		if (type === null && tendance === null) {
-		alert ("Tg");
+		alert ("O_O");
 	}
 	else{
 		alert("Le type est " + type + " . La tendance est " + tendance + " . ");
